3bf03354fa37714b1ee1d20c3dfeac67e8632530
Jeffrey_Yang <gracyme123@hotmail.com>
2019-02-13 23:35:43 +0800

    Exercise for Chapter17

--- Chapter17/exec01.c
+++ Chapter17/exec01.c
@@ -1,1 +1,99 @@
 // Modify Listing 17.2 so that it displays the movie list both in the original order and in reverse order. One approach is to modify the linked-list definition so that the list can be traversed in both directions. Another approach is to use recursion
+#include<stdio.h>
+#include<stdlib.h>    /* has the malloc prototype */
+#include<string.h>    /* has the strcpy prototype */
+#define TSIZE 45      /* size of array to hold title */
+
+struct film {
+    char title[TSIZE];
+    int rating;
+    struct film * next;    /* points to next struct in list */
+    struct film * prev;    /* points to prev struct in list */
+};
+char * s_gets(char * st, int n);
+
+int main(void)
+{
+    struct film * head = NULL;
+    struct film * prev, * current;
+    char input[TSIZE];
+
+    /* Gather and store information */
+    puts("Enter first movie title:");
+    while (s_gets(input, TSIZE) != NULL && input[0] != '\0')
+    {
+        current = (struct film *) malloc(sizeof(struct film));
+        if (head == NULL)    /* first structure */
+        {
+            head = current;
+            head->prev = NULL;
+        }   
+        else                 /* subsequent structures */
+        {
+            prev->next = current;
+            current->prev = prev;
+        }
+        current->next = NULL;
+        strcpy(current->title, input);
+        puts("Enter your rating <0-10>:");
+        scanf("%d", &current->rating);
+        while (getchar() != '\n') continue;
+        puts("Enter next movie title (empty line to stop):");
+        prev = current;
+    }
+
+    /* Show list of movies */
+    if (head == NULL)
+        printf("No data entered. ");
+    else
+        printf("Here is the movie list:\n");
+    current = head;
+    while (current != NULL)
+    {
+        printf("Movie: %s Rating: %d\n", current->title, current->rating);
+        current = current->next;
+    }
+
+    current = head;
+    /* Show list of movies in reverse order */
+    puts("\nReverse order of film.");
+    while (current->next != NULL)
+        current = current->next;
+    while (current != NULL)
+    {
+        printf("Movie: %s Rating: %d\n", current->title, current->rating);
+        current = current->prev;
+    }
+
+    /* Program done, so free allocated memory */
+    current = head;
+    while (current != NULL)
+    {
+        free(current);
+        current = current->next;
+    }
+    printf("Bye!\n");
+
+    return 0;
+}
+
+char * s_gets(char * st, int n)
+{
+    char * ret_val;
+    char * find;
+
+    ret_val = fgets(st, n, stdin);
+    if (ret_val)
+    {
+        find = strchr(st, '\n');
+        if (find)
+            *find = '\0';
+        else
+        {
+            while (getchar() != '\n')
+                continue;
+        }
+    }
+
+    return ret_val;
+}
---
+++ Chapter17/exec03.c
@@ -0,0 +1,89 @@
+/* Suppose list.h (Listing 17.3) uses the following definition of a list:
+#define MAXSIZE 100
+typedef struct list
+{
+    Item entries[MAXSIZE];
+    int items;
+} List;
+Rewrite the list.c (Listing 17.5) functions to fit this definition and test the resulting code with the films3.c (Listing 17.4) program. */
+#include<stdio.h>
+#include<stdlib.h>  /* prototype for exit()  */
+#include "list_exec03.h"   /* defines List, Item */
+void showmovies(Item item);
+char * s_gets(char * st, int n);
+int main(void)
+{
+    List movies;
+    Item temp;
+
+    /* initialize    */
+    InitializeList(&movies);
+    if (ListIsFull(&movies))
+    {
+        fprintf(stderr, "No memory available! Bye!\n");
+        exit(1);
+    }
+
+    /* gather and store */
+    puts("Enter first movie title:");
+    while (s_gets(temp.title, TSIZE) != NULL && temp.title[0] != '\0')
+    {
+        puts("Enter your rating <0-10>:");
+        scanf("%d", &temp.rating);
+        while (getchar() != '\n')
+            continue;
+        if (AddItem(temp, &movies) == false)
+        {
+            fprintf(stderr, "Problem allocating memory!\n");
+            break;
+        }
+        if (ListIsFull(&movies))
+        {
+            puts("The list is now full.");
+            break;
+        }
+        puts("Enter next movie title (empty line to stop):");
+    }
+
+    /* display     */
+    if (ListIsEmpty(&movies))
+        printf("No data entered. ");
+    else
+    {
+        printf("Here is the movie list:\n");
+        Traverse(&movies, showmovies);
+    }
+    printf("You entered %d movies.\n", ListItemCount(&movies));
+
+    /* clean up        */
+    EmptyTheList(&movies);
+    printf("Bye!\n");
+
+    return 0;
+}
+
+void showmovies(Item item)
+{
+    printf("Movie: %s Rating: %d\n", item.title, item.rating);
+}
+
+char * s_gets(char * st, int n)
+{
+    char * ret_val;
+    char * find;
+
+    ret_val = fgets(st, n, stdin);
+    if (ret_val)
+    {
+        find = strchr(st, '\n');
+        if (find)
+            *find = '\0';
+        else
+        {
+            while (getchar() != '\n')
+                continue;
+        }
+    }
+
+    return ret_val;
+}
